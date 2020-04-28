92804b1fd5391e2acb525e82dcf6602bf05d8df9
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-04-28 16:41:00 +0300

    second

--- 222.js
+++ 222.js
@@ -1,24 +1,45 @@
 
-function calcLongestSequence(n) {
-	let currentlength = 0;
-	let maxLength = 0;
-	let currentNumber = 0;
-	let previousNumber = 0;
-	let i = 0;
-	 while(currentNumber = n % 10) {
-    if (n % 10 === previousNumber) {
-        currentlength++
-        previousNumber = currentNumber;
-        maxLength = currentlength;
+// function calcLongestSequence(n) {
+// 	let currentlength = 0;
+// 	let maxLength = 1;
+// 	let currentNumber = 0;
+// 	let previousNumber = 0;
+// 	let i = 0;
+// 	 while(currentNumber = n % 10) {
+//     if (n % 10 === previousNumber) {
+//         currentlength++
+//         previousNumber = currentNumber;
+//         maxLength = currentlength;
+//     }
+//     else {
+//     	currentlength = 1;
+//     	previousNumber = currentNumber;
+//     }
+//     n = Math.floor(n / 10);
+//     i++
+//   }
+//   	return maxLength
+// }
+
+// console.log(calcLongestSequence(33333354445555));
+
+
+function numberDivisors(n) {
+    let result = 0;
+    let currentNumber = n;
+    let i = 0;
+
+    while(currentNumber >= 1) {
+        if( n / currentNumber === Math.round(n / currentNumber)) {
+            result++
+        }
+        currentNumber--
+        i++
     }
-    else {
-    	currentlength = 1;
-    	previousNumber = currentNumber;
-    }
-    n = Math.floor(n / 10);
-    i++
-  }
-  	return maxLength
+    return result
 }
 
-console.log(calcLongestSequence(33333354445555));
+console.log(numberDivisors(40));
+
+
+
