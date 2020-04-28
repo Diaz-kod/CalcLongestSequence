b610980cb0bcc8399772a86dc00832d93a50bb50
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-04-28 14:16:22 +0300

    CalcLongestSequence

---
+++ 222.js
@@ -0,0 +1,24 @@
+
+function calcLongestSequence(n) {
+	let currentlength = 0;
+	let maxLength = 0;
+	let currentNumber = 0;
+	let previousNumber = 0;
+	let i = 0;
+	 while(currentNumber = n % 10) {
+    if (n % 10 === previousNumber) {
+        currentlength++
+        previousNumber = currentNumber;
+        maxLength = currentlength;
+    }
+    else {
+    	currentlength = 1;
+    	previousNumber = currentNumber;
+    }
+    n = Math.floor(n / 10);
+    i++
+  }
+  	return maxLength
+}
+
+console.log(calcLongestSequence(33333354445555));
