a7c8c48845f9b858bbd9e2bb881e2aa3555d5a75
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-04-28 16:59:40 +0300

    second

--- 222.js
+++ 222.js
@@ -1,27 +1,25 @@
 
-// function calcLongestSequence(n) {
-// 	let currentlength = 0;
-// 	let maxLength = 1;
-// 	let currentNumber = 0;
-// 	let previousNumber = 0;
-// 	let i = 0;
-// 	 while(currentNumber = n % 10) {
-//     if (n % 10 === previousNumber) {
-//         currentlength++
-//         previousNumber = currentNumber;
-//         maxLength = currentlength;
-//     }
-//     else {
-//     	currentlength = 1;
-//     	previousNumber = currentNumber;
-//     }
-//     n = Math.floor(n / 10);
-//     i++
-//   }
-//   	return maxLength
-// }
-
-// console.log(calcLongestSequence(33333354445555));
+function calcLongestSequence(n) {
+	let currentlength = 0;
+	let maxLength = 1;
+	let currentNumber = 0;
+	let previousNumber = 0;
+	let i = 0;
+	 while(currentNumber = n % 10) {
+    if (n % 10 === previousNumber) {
+        currentlength++
+        previousNumber = currentNumber;
+        maxLength = currentlength;
+    }
+    else {
+    	currentlength = 1;
+    	previousNumber = currentNumber;
+    }
+    n = Math.floor(n / 10);
+    i++
+  }
+  	return maxLength
+}
 
 
 function numberDivisors(n) {
